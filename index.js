1beb1d7b1d76eacda4c29000b945a75b85d768a9
javierlga <javierlgac@gmail.com>
2022-08-23 14:34:25 -0600

    Removed ops

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const mysql = require('mysql')
+
+const PORT = process.env.PORT || 3000
+const HOST = '0.0.0.0'
+
+console.log(process.env)
+
+const api = express()
+api.get('/', (req, res) => {
+  res.send('PROD-193\n')
+})
+
+api.listen(PORT, HOST)
+console.log(`Running on http://${HOST}:${PORT}`)
