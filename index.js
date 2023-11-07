908561d14b8b4ab278d05a312ae0ea3e663d724c
Kyle Campbell <kc@kc.io>
2023-11-07 08:40:29 -0800

    PROD-119

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000
 const HOST = '0.0.0.0'
 
 // !* Edit here for demos
-const RELEASE_NO = 'PROD-118'
+const RELEASE_NO = 'PROD-119'
 
 const api = express()
 
@@ -23,3 +23,42 @@ api.get('/', (req, res) => {
 api.listen(PORT, HOST)
 console.log(`Running on http://${HOST}:${PORT}`)
 
+class Vehicle {
+  constructor(make, model, year) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+  }
+
+  displayDetails() {
+    console.log(`Make: ${this.make}`);
+    console.log(`Model: ${this.model}`);
+    console.log(`Year: ${this.year}`);
+  }
+}
+
+class Car extends Vehicle {
+  constructor(make, model, year, doors) {
+    super(make, model, year);
+    this.doors = doors;
+  }
+
+  displayDetails() {
+    super.displayDetails();
+    console.log(`Doors: ${this.doors}`);
+  }
+}
+
+// Create an instance of the Vehicle class
+const vehicle = new Vehicle('Ford', 'F-150', 2020);
+
+// Display vehicle details
+console.log('Vehicle Details:');
+vehicle.displayDetails();
+
+// Create an instance of the Car class
+const car = new Car('Honda', 'Accord', 2023, 4);
+
+// Display car details
+console.log('\nCar Details:');
+car.displayDetails();
