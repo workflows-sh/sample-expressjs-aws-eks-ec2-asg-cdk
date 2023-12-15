76d1ff561ed446369b83b7fa18aeaeb7e7e81d0e
Kyle Campbell <kc@kc.io>
2023-12-15 14:37:02 -0800

    Fix syntax error

--- index.js
+++ index.js
@@ -20,29 +20,5 @@ api.get('/', (req, res) => {
   })
 })
 
-class Person {
-    name: string;
-    
-    constructor(name: string) {
-        this.name = name;
-    }
-}
-
-class Employee extends Person {
-    empCode: number;
-    
-    constructor(empcode: number, name:string) {
-        super(name);
-        this.empCode = empcode;
-    }
-    
-    displayName():void {
-        console.log("Name = " + this.name +  ", Employee Code = " + this.empCode);
-    }
-}
-
-let emp = new Employee(100, "Bill");
-emp.displayName();
-
 api.listen(PORT, HOST)
 console.log(`Running on http://${HOST}:${PORT}`)
