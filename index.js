4a811096df46c8db30a2136e820b3f185c215c30
Kyle Campbell <kc@kc.io>
2023-12-18 11:18:35 -0800

    cat example

--- index.js
+++ index.js
@@ -20,5 +20,41 @@ api.get('/', (req, res) => {
   })
 })
 
+/**
+ * Represents a Cat object.
+ */
+class Cat {
+    /**
+     * Constructor for the Cat class.
+     *
+     * @param {string} name - The name of the cat.
+     */
+    constructor(name) {
+        this.name = name;
+    }
+
+    /**
+     * Makes the cat "meow".
+     */
+    meow() {
+        console.log(`${this.name} says meow!`);
+    }
+}
+
+/**
+ * Creates a new Cat object with the given name.
+ *
+ * @param {string} name - The name of the cat.
+ * @returns {Cat} The created Cat object.
+ */
+function makeCat(name) {
+    return new Cat(name);
+}
+
+// Usage Example for makeCat function
+
+const myCat = makeCat("Dot");
+myCat.meow();  // Output: Dot says meow!
+
 api.listen(PORT, HOST)
 console.log(`Running on http://${HOST}:${PORT}`)
