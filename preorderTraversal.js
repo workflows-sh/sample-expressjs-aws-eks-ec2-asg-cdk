332fc6bda7294060b8f8553f9fe5200a6de62467
CampbellBrett <mail@brett-campbell.com>
2024-05-14 21:40:53 -0300

    Update preorderTraversal.js

--- preorderTraversal.js
+++ preorderTraversal.js
@@ -7,8 +7,8 @@ function preorderTraversal(root) {
   let result = [];
   if (root !== null) {
     result.push(root.val);
-    preorderTraversal(root.left);
-    preorderTraversal(root.right);
+    result.push(...preorderTraversal(root.left));
+    result.push(...preorderTraversal(root.right));
   }
   return result;
 }
@@ -16,4 +16,4 @@ function preorderTraversal(root) {
 module.exports = {
   TreeNode,
   preorderTraversal
-}
+};
